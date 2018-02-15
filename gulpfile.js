8c9e7464fb5f25cafc43b60b022168a1a00823bd
Shaun Teeshad <shaun.teeshad@gmail.com>
2018-02-15 16:55:24 -0500

    Finished the layout

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested'); 
+var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
+var mixins = require('postcss-mixins');
+
+gulp.task('html', function() {
+    console.log("You creeated a gulp task - html");
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins,  cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
